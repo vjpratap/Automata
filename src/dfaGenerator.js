b31d16afdbde1b1ef3dc66802a4548c6756548bf
vjpratap <vijayprsingh619@gmail.com>
2016-06-07 16:52:06 +0530

    [Automata] | changing the name of variable to make the code more readeble

--- src/dfaGenerator.js
+++ src/dfaGenerator.js
@@ -6,7 +6,7 @@ var lodash = require('lodash')
 generators.dfaGenerator = function(touple){
 	return function(string){
 		var charString = string.split("");
-		if(!stringVarify(charString, touple.alphabet,string)){
+		if(isStringContainMoreCharacterThanAlphabets(charString, touple.alphabet,string)){
 			return false;
 		}
 		var finalStateOfString = charString.reduce(function (currentState, character){
@@ -19,24 +19,28 @@ generators.dfaGenerator = function(touple){
 generators.nfaGenerator = function(touple){
 	return function(string) {
 		var charString = string.split("")
-		if(!stringVarify(charString, touple.alphabet, string))
+		if(isStringContainMoreCharacterThanAlphabets(charString, touple.alphabet, string))
 			return false
-		var finalStateOfString = charString.reduce(function (currentStates, character){
-			var currentStatesArrays = currentStates.map(function(currentState){
+		var finalStatesOfString = charString.reduce(function (currentStates, character){
+			var currentStatesSets = currentStates.map(function(currentState){
 				return touple.transitionFunction[currentState][character]
 			})
-			return currentStatesArrays.reduce(getAllCurrentStates)
+			return currentStatesSets.reduce(getAllCurrentStates)
 		}, touple.initialState)
-		return lodash.intersection(finalStateOfString, touple.finalState).length > 0;
+		return isStringFinalStatesContainsFinalState(finalStatesOfString, touple.finalState);
 	}
+}
+
+var isStringFinalStatesContainsFinalState = function(finalStatesOfString, finalStates){
+	return lodash.intersection(finalStatesOfString, finalStates).length > 0;
 }
 
 var getAllCurrentStates = function(previousValue, currentValue){
 	return lodash.union(previousValue, currentValue)
 }
 
-var stringVarify = function(charString, alphabet,string){
+var isStringContainMoreCharacterThanAlphabets = function(charString, alphabet,string){
 	var charString = string.split("");
 	var regex = "^[" + alphabet.join("") + "]*$"
-	return string.match(regex)
+	return !string.match(regex)
 }
